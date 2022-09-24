b26cfba88fc5cde0b53dc9cab9609822976486fe
Jordan Phillips <jordypee27@gmail.com>
2022-09-24 10:49:07 +1000

    feat: add common infrastructure library

---
+++ src/Infrastructure/src/Infrastructure/Exceptions/DomainException.cs
@@ -0,0 +1,13 @@
+﻿namespace Giantnodes.Infrastructure.Exceptions
+{
+    public class DomainException<T> : Exception
+    {
+        public T Result { get; private set; } = default!;
+
+        public DomainException(T error, string? message = null)
+            : base(message ?? "An error occurred")
+        {
+            Result = error;
+        }
+    }
+}
---
+++ src/Infrastructure/src/Infrastructure/Extensions/EnumExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace Giantnodes.Infrastructure.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetStringValue(this Enum value)
+        {
+            Type type = value.GetType();
+
+            FieldInfo? info = type.GetField(value.ToString());
+            DescriptionAttribute[] attribute = info?.GetCustomAttributes(typeof(DescriptionAttribute), false) as DescriptionAttribute[]
+                ?? Array.Empty<DescriptionAttribute>();
+
+            return attribute.Any() ? attribute.First().Description : string.Empty;
+        }
+    }
+}
