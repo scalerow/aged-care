3d4aea10034389534a08a4f62b3ba532ec9d4a08
Jordan Phillips <jordypee27@gmail.com>
2022-10-03 18:36:49 +1100

    tests(tenants): add application and shared tests projects

---
+++ src/Servcie.Tenants/tests/Application.Tests/ApplicationTestsServiceRegistration.cs
@@ -0,0 +1,14 @@
+﻿using Giantnodes.Infrastructure.Mail;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Giantnodes.Service.Tenants.Application.Tests
+{
+    public static class ApplicationTestsServiceRegistration
+    {
+        public static IServiceCollection AddApplicationTestServices(this IServiceCollection services)
+        {
+            services.AddMailTestsServices();
+            return services;
+        }
+    }
+}
---
+++ src/Servcie.Tenants/tests/Shared.Tests/Bogus/TenantFaker.cs
@@ -0,0 +1,14 @@
+﻿using Bogus;
+using Giantnodes.Service.Tenants.Domain.Entities;
+
+namespace Giantnodes.Service.Tenants.Shared.Tests.Bogus
+{
+    public class TenantFaker : Faker<Tenant>
+    {
+        public TenantFaker()
+        {
+            RuleFor(p => p.Id, f => f.Random.Uuid());
+            RuleFor(p => p.Name, f => f.Company.CompanyName());
+        }
+    }
+}
